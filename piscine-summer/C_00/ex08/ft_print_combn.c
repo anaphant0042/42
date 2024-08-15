5e5117d3a9a8da9f36217cbf8a7b31690f4e09e9
Ana Lucia Lara Gutierrez De Velazco <anlara-g@cbr8s2.42barcelona.com>
2024-08-15 14:32:57 +0200

    c00 ex08 accomplished!

--- piscine-summer/C_00/ex08/ft_print_combn.c
+++ piscine-summer/C_00/ex08/ft_print_combn.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: anlara-g <anlara-g@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/08/13 14:42:04 by anlara-g          #+#    #+#             */
-/*   Updated: 2024/08/13 14:57:13 by anlara-g         ###   ########.fr       */
+/*   Created: 2024/08/15 14:29:16 by anlara-g          #+#    #+#             */
+/*   Updated: 2024/08/15 14:32:26 by anlara-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,8 @@
 #include <stdio.h>
 
 void	ft_print_combn(int n);
-
+void	write_comma(int i, int j, int n);
+void	write_i(int i);
 
 int	main(void)
 {
@@ -28,22 +29,43 @@ void	ft_print_combn(int n)
 {
 	int		i;
 	int		j;
+	int		k;
+	int		m;
+
+	k = 0;
+	i = 0;
+	j = i + (n - 1);
+	while (k <= 10 - n)
+	{
+		i = k;
+		m = 0;
+		while (m++ < n - 1)
+			write_i(i++);
+		write_i(j);
+		write_comma(k, j, n);
+		if (j == 9)
+		{
+			k++;
+			j = k + (n - 1);
+		}
+		else
+			j++;
+	}
+}
+
+void	write_comma(int k, int j, int n)
+{
+	if (k == 10 - n && j == 9)
+	{
+	}
+	else
+		write(1, ",", 1);
+}
+
+void	write_i(int i)
+{
 	char	c;
 
-	i = 0;
-	j = 0;
-	while (i < n)
-	{
-		c = i + '0';
-		write(1, &c, 1);
-		j = i + 1;
-		while (j < 10)
-		{
-				c = j + '0';
-				write(1, &c, 1);
-				j++;
-		}
-		write(1, ",", 1);
-		i++;
-	}
+	c = i + '0';
+	write(1, &c, 1);
 }
