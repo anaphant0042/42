c4600b8e54affd76e61b620e60b01db5d80b85c8
Ana Lucia Lara Gutierrez De Velazco <anlara-g@cbr9s2.42barcelona.com>
2024-08-19 16:13:41 +0200

    c02 ex09 superado

--- piscine-summer/C_02/ex09/ft_strcapitalize.c
+++ piscine-summer/C_02/ex09/ft_strcapitalize.c
@@ -6,7 +6,7 @@
 /*   By: anlara-g <anlara-g@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/15 16:26:35 by anlara-g          #+#    #+#             */
-/*   Updated: 2024/08/15 16:41:34 by anlara-g         ###   ########.fr       */
+/*   Updated: 2024/08/19 16:12:25 by anlara-g         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,16 +23,18 @@ char	*ft_strcapitalize(char *str)
 	{
 		if (str[i] >= 'A' && str[i] <= 'Z')
 		{
-			printf("entering mayus loop");
 			str[i] = str[i] + 32;
 			i++;
 		}
-		else if (str[i] < 48 || (str[i] > 90  || str[i] < 97 ) ||  (str[i] > 123  || str[i] < 128))
+		else if (str[i] >= 32 && str[i] <= 64)
 		{
-			printf("str[%i]: %i\n", i, str[i]);
-			if (str[i + 1] >= 'a' && str[i + 1] <= 'z')
+			if (str[i + 1] >= 'a' && str[i + 1] <= 'z' && str[i + 1])
+			{
 				str[i + 1] = str[i + 1] - 32;
-			i = i + 2;
+				i = i + 2;
+			}
+			else
+				i++;
 		}
 		else
 			i++;
@@ -43,11 +45,9 @@ char	*ft_strcapitalize(char *str)
 int	main(int argc, char **argv)
 {
 	char	*str = argv[1];
-	char	*capitalized_str;
 
 	if (argc < 2)
 		return(0);
 	else
-		capitalized_str = ft_strcapitalize(str);
-	printf("%s\n", capitalized_str);
+			printf("%s\n", ft_strcapitalize(str));
 }
